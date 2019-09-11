0c00578023e0c987c9c9b0c2720320c91432ea55
Bikash Nandi <bv.bikash@gmail.com>
2019-09-11 14:57:00 +0530

    FormBuilder service and form Validation in reactive form

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
@@ -9,12 +9,27 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class CreateEmployeeComponent implements OnInit {
 
   employeeForm: FormGroup;
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.employeeForm = new FormGroup({
+    /*this.employeeForm = new FormGroup({
       fullName: new FormControl(),
-      email: new FormControl()
+      email: new FormControl(),
+      skills: new FormGroup({
+        skillName: new FormControl(),
+        experienceInYears: new FormControl(),
+        proficiency: new FormControl()
+      })
+    });*/
+    // formBuilder is a predefied service used to create form group and control
+    this.employeeForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: [''],
+      skills: this.fb.group({
+        skillName: [''],
+        experienceInYears: [''],
+        proficiency:['intermediate']
+      })
     });
   }
 
@@ -25,4 +40,37 @@ export class CreateEmployeeComponent implements OnInit {
     console.log(this.employeeForm.get('email').value);
   }
 
+  // patchValue can be used to set partial value where as set value can only be used to set entire form value
+  onLoadDataClick() {
+    /*this.employeeForm.setValue({
+      fullName: 'James',
+      email: 'james112@outlook.com',
+      skills: {
+        skillName: 'Painter',
+        experienceInYears: 1,
+        proficiency: 'beginer'
+      }
+    });*/
+    this.employeeForm.patchValue({
+      fullName: 'James',
+      email: 'james112@outlook.com'
+    });
+
+    this.logKeyValuePair(this.employeeForm);
+  }
+
+  logKeyValuePair(group: FormGroup) {
+    Object.keys(group.controls).forEach((key: string) =>{
+      const abstractControl = group.get(key);
+      if( abstractControl instanceof FormGroup) {
+        this.logKeyValuePair(abstractControl);
+      } else {
+        console.log('key = ' + key + ', value = ' + abstractControl.value);
+      }
+    });
+  }
+
+  onDisableClick()  {
+    this.employeeForm.disable();
+  }
 }
